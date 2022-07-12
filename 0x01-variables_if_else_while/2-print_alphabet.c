da408c8eb1857a8df907ffd66e6e1f636a5bb8c3
David Mbacha <davidmbacha@gmail.com>
2022-07-12 11:17:39 -0700

    fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  main - main block
- *  Description: Get a random number and check its last digit, compare it with 5
- *  Return: 0
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		putchar(c);		
+		putchar(c);
 		c++;
 	}
 
