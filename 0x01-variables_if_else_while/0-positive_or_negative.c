73d33eeab440f0896979097ce04d9ee32343d5d1
David Mbacha <davidmbacha@gmail.com>
2022-07-12 09:39:21 -0700

    fixed description

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 /*
  * main - Entry point
- *
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
  * Return: Always 0 (Success)
  */
 int main(void)
